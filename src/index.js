c1bd105a2520e81175f11a83a5d9a6081f78b5e5
@960bg Mihail Dementev <-h>
2022-08-12 19:15:12 +0400

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,42 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix = []) {
+  // console.log(matrix);
+  let mas = [];
+
+  for (i = 0; i < matrix.length; i++) {
+    for (j = 0; j < matrix[i].length; j++) {
+      if (i % 2 == 0) {
+        // mas.push(matrix[i].sort((a, b) => { return (+a) - (+b) }));
+        mas.push([...(matrix[i].sort((a, b) => { return (+a) - (+b) }))]);
+        break;
+      } else {
+        // mas.push(matrix[i].sort((a, b) => { return (+b) - (+a) }));
+        mas.push([...(matrix[i].sort((a, b) => { return (+b) - (+a) }))]);
+        break;
+      }
+    }
+
+  }
+  // mas = mas.sort((a, b) => { return (+a) - (+b) });
+  console.log(mas.flat(Infinity));
+  return mas.flat(Infinity);
 }
+
+
+
+// towelSort([[1, 2, 3, 4],
+// [5, 6, 7, 8],
+// [9, 10, 11, 12],
+// [13, 14, 15, 16]
+// ]);
+
+// [
+//   [ 1, 2, 3, 4 ],
+//   [ 5, 6, 7, 8 ],
+//   [ 9, 10, 11, 12 ],
+//   [ 13, 14, 15, 16 ]
+// ]
+
+// [ [ 1, 2, 4 ], [ 5, 6, 7, 8 ], [ 9, 12 ] ]
